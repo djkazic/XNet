4df6475670232a5c4aa8a0d5221f71dd9cb2e75e
djkazic <kevincai96@gmail.com>
2015-02-13 11:32:20 -0500

    Created gui -- MainWindow

---
+++ src/gui/MainWindow.java
@@ -0,0 +1,56 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import main.Core;
+
+public class MainWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow frame = new MainWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainWindow() {
+		setTitle("XNet v" + Core.version);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 231, 325, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnEnter = new JButton("Enter");
+		btnEnter.setBounds(349, 230, 75, 23);
+		contentPane.add(btnEnter);
+	}
+}
