1d869032a62555b9235d1706578035e7f44c2afd
djkazic <kevincai96@gmail.com>
2015-02-23 12:18:26 -0500

    Reactivated double-user debugging

--- src/main/Core.java
+++ src/main/Core.java
@@ -87,19 +87,19 @@ public class Core {
 		mainWindow.searchInput.setEditable(true);
 		
 		//Local development tools
-		debugServer = false;
-		int sep = 1;
+		//debugServer = false;
+		//int sep = 1;
 		
-		if(sep == 0) {
+		//if(sep == 0) {
 			gl = new GlobalListener();
 			(new Thread(gl)).start();
-		} else {
+		//} else {
 			pst = new PeerConnector(debugServer);
-			Core.potentialPeers.add("127.0.0.1");
+		//	Core.potentialPeers.add("127.0.0.1");
 			(new Thread(pst)).start();
 			//TODO: remove debugging
-			Core.discoveryLatch.countDown();
-		}
+		//	Core.discoveryLatch.countDown();
+		//}
 	}
 	
 	public static void incomingDebugReset() {
