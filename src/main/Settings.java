f0a2224b4b3ff9e3fc25fac2c3f0a3f0d7140e85
djkazic <kevincai96@gmail.com>
2015-02-23 18:36:51 -0500

    Doubled block size to 256kb

--- src/main/Settings.java
+++ src/main/Settings.java
@@ -4,6 +4,6 @@ public class Settings {
 
 	public static double version;
 	public static String hwid = "";
-	public static long blockSize = 128000; //128kb blocks
+	public static long blockSize = 256000; //256kb blocks
 
 }
