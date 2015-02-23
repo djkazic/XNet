dcba48614402cf62212dffc05e946f0bd047f02b
djkazic <kevincai96@gmail.com>
2015-02-23 13:16:26 -0500

    Added deleteDirectory in Utils

--- src/main/Utils.java
+++ src/main/Utils.java
@@ -460,4 +460,17 @@ public class Utils {
 		}
 		return "[" + inCount + "|" + outCount + "]";
 	}
+	
+	public static void deleteDirectory(File path) {
+	    if (path.exists()) {
+	        File[] files = path.listFiles();
+	        for (int i = 0; i < files.length; i++) {
+	            if (files[i].isDirectory()) {
+	                deleteDirectory(files[i]);
+	            } else {
+	                files[i].delete();
+	            }
+	        }
+	    }
+	}
 }
