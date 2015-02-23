c7d9f0b43eb8eaffe60705f8dd6f8b4d52a3ef71
djkazic <kevincai96@gmail.com>
2015-02-23 16:53:17 -0500

    Shortened SenderThread sleep time

--- src/net/SenderThread.java
+++ src/net/SenderThread.java
@@ -99,7 +99,7 @@ public class SenderThread implements Runnable {
 					disconnect = false;
 				}
 				try {
-					Thread.sleep(25);
+					Thread.sleep(20);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
